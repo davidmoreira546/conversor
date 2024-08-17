38d55b795766cc5fad3c4df1ead3ae5b0d00eba7
david <hunter54607@gmail>
2024-08-17 19:24:57 -0300

    nova mod

--- teste.py
+++ teste.py
@@ -1,10 +1,18 @@
 import os #conversao basica de arquivos de video e audio. ex:mp3,mp4,ts,mov
 import aspose.words as aw #conversao de imagem e pdfs
 
-def converter_video(arquivo,dir):
-    option = os.system('ls')
-    for i in range(option):
-        print(f'{i}')
-    print('insira diretorio')
-    dir = input(' ')
-    arquivo = len
+def converter_video():
+    print(os.system('ls'))
+    print('insira o nome do arquivo')
+    arquivo = input(" ")
+    print('formado\nmov\nmp4\nmp3\nts')
+    tipo =  input(" ")
+    os.rename(f'{arquivo}.{tipo}' )
+    print('arquivo convertido')
+
+while True:
+    print('que tipo de arquivo\n[1]video')
+    r = int(input(' '))
+    if r == 1:
+        converter_video()
+
